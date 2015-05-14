177d3b7da180edbf37cf8b52caf7fa03bbcdce04
Eric S. Talbot <ericstalbot@gmail.com>
2015-05-14 01:13:19 +0000

    initial settings changes for pa

--- pasite/deployment_settings.py
+++ pasite/deployment_settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open('../../pa_secret_key.txt').read().strip()
+SECRET_KEY = open('/home/ericstalbot/pasite_secret_key.txt').read().strip()
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
